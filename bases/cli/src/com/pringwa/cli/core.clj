53c0f23d8ea0cd68eb5bcd3cf1312755e3b26f12
Simon Okwori <simon.okwori@gmail.com>
2026-02-08 13:51:50 -0600

    Main method for base poly/cli

--- bases/cli/src/com/pringwa/cli/core.clj
+++ bases/cli/src/com/pringwa/cli/core.clj
@@ -1,2 +1,8 @@
-(ns com.pringwa.cli.core)
+(ns com.pringwa.cli.core
+  (:require [com.pringwa.user.interface :as user])
+  (:gen-class))
 
+
+(defn -main [& args]
+  (println (user/hello (first args)))
+  (System/exit 0))
