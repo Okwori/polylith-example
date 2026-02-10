1db494038cf179e3c3a37f1914be121dc43cedfb
Simon Okwori <simon.okwori@gmail.com>
2026-02-10 09:51:13 -0600

    Initialise persistence component

---
+++ components/persistence/test/com/pringwa/persistence/interface_test.clj
@@ -0,0 +1,6 @@
+(ns com.pringwa.persistence.interface-test
+  (:require [clojure.test :as test :refer :all]
+            [com.pringwa.persistence.interface :as persistence]))
+
+(deftest dummy-test
+  (is (= 1 1)))
