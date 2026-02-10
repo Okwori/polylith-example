a04c0b5a918324f024cb21b1ff8c3218e39c7f88
Simon Okwori <simon.okwori@gmail.com>
2026-02-10 13:39:55 -0600

    Add app-state component

---
+++ components/app_state/src/com/pringwa/app_state/interface.clj
@@ -0,0 +1,1 @@
+(ns com.pringwa.app-state.interface)
---
+++ components/app_state/test/com/pringwa/app_state/interface_test.clj
@@ -0,0 +1,6 @@
+(ns com.pringwa.app-state.interface-test
+  (:require [clojure.test :as test :refer :all]
+            [com.pringwa.app-state.interface :as app-state]))
+
+(deftest dummy-test
+  (is (= 1 1)))
