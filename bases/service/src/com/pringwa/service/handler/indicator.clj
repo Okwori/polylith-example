736a57c270b28ded100ab2d2e7e1752e9a5e7f4c
Simon Okwori <simon.okwori@gmail.com>
2026-02-10 18:44:54 -0600

    Test DB conn

--- bases/service/src/com/pringwa/service/handler/indicator.clj
+++ bases/service/src/com/pringwa/service/handler/indicator.clj
@@ -1,7 +1,9 @@
-(ns com.pringwa.service.handler.indicator)
+(ns com.pringwa.service.handler.indicator
+  (:require [datomic.client.api :as d]))
 
 (defn handler
-  [{:keys [reitit.core/match]}]
+  [{:keys [reitit.core/match conn]}]
   (let [id (-> match :path-params :id Long/parseLong)]
-   {:status 200
-    :body   {:result id}}))
+    {:status 200
+    :body   {:result id
+             :db-value-from-conn (d/q '[:find ?e :where [?e :db/ident]] (d/db conn))}}))
