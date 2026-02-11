c92bfdd5235b91b84978caec6eb742ef22c75129
Simon Okwori <simon.okwori@gmail.com>
2026-02-11 12:07:58 -0600

    Type parameter checking

--- bases/service/src/com/pringwa/service/handler/indicators.clj
+++ bases/service/src/com/pringwa/service/handler/indicators.clj
@@ -3,8 +3,12 @@
             [datomic.client.api :as d]))
 
 (defn handler
-  [{:keys [conn]}]
-  (let [conn (:conn (store/init-db))]
+  [{:keys [query-params] :as req}]
+  (let [conn (:conn (store/init-db))
+        type (not-empty (get query-params "type"))]
     {:status 200
-     :body   {:result (-> (store/findAllDocuments (d/db conn))
-                          store/transform-keys)}}))
+     :body   (if type
+               {:result (-> (store/find-document-by-type (d/db conn) type)
+                            store/transform-keys)}
+               {:result (-> (store/find-all-documents (d/db conn))
+                            store/transform-keys)})}))
