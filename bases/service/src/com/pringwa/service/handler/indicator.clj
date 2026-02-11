3234d4bfd6049500d7475cc50aebd1fb4df1afef
Simon Okwori <simon.okwori@gmail.com>
2026-02-10 19:48:52 -0600

    Playground

--- bases/service/src/com/pringwa/service/handler/indicator.clj
+++ bases/service/src/com/pringwa/service/handler/indicator.clj
@@ -2,8 +2,10 @@
   (:require [datomic.client.api :as d]))
 
 (defn handler
-  [{:keys [reitit.core/match conn]}]
+  [{:keys [reitit.core/match app-state] :as req}]
   (let [id (-> match :path-params :id Long/parseLong)]
     {:status 200
     :body   {:result id
-             :db-value-from-conn (d/q '[:find ?e :where [?e :db/ident]] (d/db conn))}}))
+             :db-value-from-conn (keys req)
+             ;(d/q '[:find ?e :where [?e :db/ident]] (d/db conn))
+             }}))
